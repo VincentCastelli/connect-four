83c859446fbdf29829d2da71aadcc52561000d1d
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-09-13 13:55:39 -0700

    Stood up server, generated static html, hooked in app for rendering.

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Connect Four</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
